29f896f59a5f5c4e8cf2ce64e841734e1a59f0f2
Jack Reiter <superjack500@gmail.com>
2026-09-23 16:26:00 -0400

    index didnt upload?

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// code from google used to test
+// Using the built-in function
+echo 'Current PHP version: ' . phpversion();
+
+// Alternative method using a constant (performs slightly faster)
+// echo PHP_VERSION;
+?>
